7454954b1ae6778b942c584d7fdeb9bfc23eba91
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2017-01-08 01:43:10 +0100

    adding free server

--- rdvhome/mqtt.py
+++ rdvhome/mqtt.py
@@ -2,10 +2,10 @@
 
 def Client(on_connect = None, on_message = None):
     from paho.mqtt.client import Client as Mqtt
-
     mqtt = Mqtt()
     mqtt.on_connect = on_connect
     mqtt.on_message = on_message
-    mqtt.connect("localhost", 1883, 60)
-
+    mqtt.username_pw_set('owflekio', '0aeZc50ixVbT')
+    mqtt.connect("m21.cloudmqtt.com", 16494, 60)
+    #mqtt.connect("localhost", 1883, 60)
     return mqtt
