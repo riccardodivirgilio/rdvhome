be363d605314b39be7abb433611c0df83c09b261
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2017-01-08 01:45:24 +0100

    adding status command

---
+++ rdvhome/management/commands/status.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from rdvhome.management.mqtt import MqttCommand
+
+from django.core.management.base import BaseCommand
+from django.utils.functional import cached_property
+
+class Command(MqttCommand, BaseCommand):
+
+    def handle(self, **options):
+        self.mqtt.publish('command', '/status')
