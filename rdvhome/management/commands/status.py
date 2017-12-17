62f89d48858ddc5b4d015c49d89657dacff709ac
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2017-12-17 22:42:34 +0100

    adding missing import

--- rdvhome/management/commands/status.py
+++ rdvhome/management/commands/status.py
@@ -6,6 +6,7 @@ from django.core.management.base import BaseCommand
 from django.core.urlresolvers import reverse
 
 from rdvhome.management.mqtt import MqttCommand
+from django.conf import settings
 
 class Command(MqttCommand, BaseCommand):
 
