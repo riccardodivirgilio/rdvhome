88d39b4a3e8497ed881eed0762b242582012863d
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2017-12-22 14:42:11 +0100

    adding basic settings

---
+++ myhome/conf.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from myhome.utils.datastructures import data
+
+settings = data(
+    DEBUG = True,
+)
