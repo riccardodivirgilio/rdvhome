db85fce5f3d107711d1ef31432a102aa44393bd2
Riccardo Di Virgilio <rdv@rdv.local>
2017-12-23 23:18:40 +0100

    adding instance to window for debugging

--- rdvhome/frontend/src/main.js
+++ rdvhome/frontend/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueNativeSock from 'vue-native-websocket'
 
-new Vue({
+Vue.use(VueNativeSock, 'ws://localhost:8500/websocket');
+
+window.vm = new Vue({
   el: '#app',
-  render: function(h) {return h(App)} 
+  render: function(h) {return h(App)}
 })
