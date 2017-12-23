ddfc9e570f24bd16f3659bcd4cf4ea126fe5e2bc
Riccardo Di Virgilio <rdv@rdv.local>
2017-12-23 15:06:47 +0100

    fixing empty list

--- rdvhome/utils/async.py
+++ rdvhome/utils/async.py
@@ -9,8 +9,12 @@ from rdvhome.utils.functional import iterate
 import asyncio
 
 async def wait_all(*args):
-    done, _ = await asyncio.wait(tuple(iterate(*args)))
-    return map(methodcaller('result'), done)
+    done = tuple(iterate(*args))
+    if done:
+        done, _ = await asyncio.wait(done)
+        return map(methodcaller('result'), done)
+
+    return done
 
 def syncronous_wait_all(*args):
     yield from asyncio.get_event_loop().run_until_complete(wait_all(*args))
