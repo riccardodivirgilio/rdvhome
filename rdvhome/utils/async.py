cfe3f61a55d412a6359e79cd1826022df235e9ae
Riccardo Di Virgilio <rdv@rdv.local>
2017-12-23 14:56:10 +0100

    adding wait all utils

--- rdvhome/utils/async.py
+++ rdvhome/utils/async.py
@@ -2,10 +2,15 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-from rdvhome.utils.functional import iterate
 from operator import methodcaller
+
+from rdvhome.utils.functional import iterate
+
 import asyncio
 
 async def wait_all(*args):
     done, _ = await asyncio.wait(tuple(iterate(*args)))
     return map(methodcaller('result'), done)
+
+def syncronous_wait_all(*args):
+    yield from asyncio.get_event_loop().run_until_complete(wait_all(*args))
