8971b4d20b50700956cd906bfa6e73425dfe5648
Riccardo Di Virgilio <rdv@rdv.local>
2017-12-23 15:19:34 +0100

    using a different constructor

--- run.py
+++ run.py
@@ -6,18 +6,21 @@ if __name__ == '__main__':
 
     from rdvhome.cli.main import execute_from_command_line
 
-    router = dict(
+    philips = lambda id, name, **opts: dict(
+        id        = id,
+        name      = name,
         username  = "Ro1Y0u6kFH-vgkwdbYWAk8wQNUaXM3ODosHaHG8W",
         ipaddress = "192.168.1.179",
+        **opts
     )
 
     execute_from_command_line(
         DEBUG = True,
         SWITCHES = {
-            'rdvhome.switches.philips.PhilipsSwitch': [
-                dict(id = 'b1', name = 'Camera letto', philips_id = 2, alias = [], **router),
-                dict(id = 'l1', name = 'Salone 1',     philips_id = 1, alias = ['default'], **router),
-                dict(id = 'l2', name = 'Salone 2',     philips_id = 3, alias = ['default'], **router),
-            ]
+            'rdvhome.switches.philips.PhilipsSwitch': (
+                philips('b1', 'Camera letto', philips_id = 2, alias = []),
+                philips('l1', 'Salone 1',     philips_id = 1, alias = ['default']),
+                philips('l2', 'Salone 2',     philips_id = 3, alias = ['default']),
+            )
         }
     )
