e35a43eb66663be38ded0846db0a27056d79fe25
Riccardo Di Virgilio <rdv@rdv.local>
2017-12-23 14:24:43 +0100

    fetching status from the api

--- rdvhome/switches/philips.py
+++ rdvhome/switches/philips.py
@@ -3,6 +3,8 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from rdvhome.switches.base import Switch
+import aiohttp
+from rdvhome.utils.datastructures import data
 
 class PhilipsSwitch(Switch):
 
@@ -12,9 +14,15 @@ class PhilipsSwitch(Switch):
         self.username   = username
         super(PhilipsSwitch, self).__init__(id, **opts)
 
-    def full_url(self, url):
+    def api_url(self):
         return 'http://%s/api/%s/lights/%s' % (
             self.ipaddress,
-            self.user,
+            self.username,
             self.philips_id
         )
+
+    async def get_status(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.api_url()) as response:
+                r = await response.json()
+                return data(on = r['state']['on'])
