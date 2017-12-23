bddf9f381f944ecceeac728c74429f7b844beb7c
Riccardo Di Virgilio <rdv@rdv.local>
2017-12-23 16:59:30 +0100

    adding event logger

--- rdvhome/cli/commands/runserver.py
+++ rdvhome/cli/commands/runserver.py
@@ -5,8 +5,12 @@ from __future__ import absolute_import, print_function, unicode_literals
 from rdvhome.app import app
 from rdvhome.cli.utils import SimpleCommand
 from rdvhome.conf import settings
+from rdvhome.events import status_stream
 
 import asyncio
+
+async def log(value):
+    print(value)
 
 class Command(SimpleCommand):
 
@@ -22,12 +26,17 @@ class Command(SimpleCommand):
             aiohttp_autoreload.start()
 
         loop = asyncio.get_event_loop()
+
         loop.run_until_complete(
             loop.create_server(
                 app.make_handler(),
                 address,
                 port
             )
+        )
+
+        loop.run_until_complete(
+            status_stream.subscribe(log)
         )
 
         print('Server started at http://%s:%s' % (address, port))
