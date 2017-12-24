ce7423193beffe15b12c5016ec6a760045fc513a
Riccardo Di Virgilio <rdv@rdv.local>
2017-12-24 10:29:10 +0100

    adding to_color util

---
+++ rdvhome/utils/colors.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from colour import Color
+
+import math
+
+
+def to_color(spec):
+    if isinstance(spec, Color):
+        return spec
+    return Color(spec)
+
+
+def hsb_to_hsl(h, s, b):
+    l = 0.5 * b  * (2 - s)
+    s = b * s / (1 - math.fabs(2*l-1))
+    return Color(
+        hue        = h,
+        saturation = s,
+        luminance  = l,
+    )
