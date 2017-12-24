d4d16c0b099336c64c97b06843777884ef76c33e
Riccardo Di Virgilio <rdv@rdv.local>
2017-12-24 17:38:32 +0100

    using the very same hostname instead of localhost

--- rdvhome/frontend/src/main.js
+++ rdvhome/frontend/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueNativeSock from 'vue-native-websocket'
 
-Vue.use(VueNativeSock, 'ws://localhost:8500/websocket');
+Vue.use(VueNativeSock, 'ws://'+ window.location.hostname +':8500/websocket');
 
 window.vm = new Vue({
   el: '#app',
