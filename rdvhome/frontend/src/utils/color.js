4b6cf312b5e8a10ab3368ae8dd22602b96322fb7
Riccardo Di Virgilio <rdv@rdv.local>
2017-12-26 11:24:51 +0100

    adding color lib

--- rdvhome/frontend/src/utils/color.js
+++ rdvhome/frontend/src/utils/color.js
@@ -1,7 +1,7 @@
-export function hsb_to_hsl(h, s, v) {
-    if (arguments.length === 1) {
-        s = h.s, v = h.v, h = h.h;
-    };
+export function hsb_to_hsl(data) {
+    var s = data.saturation;
+    var v = data.brightness;
+    var h = data.hue;
 
     var _h = h,
         _s = s * v,
@@ -10,17 +10,17 @@ export function hsb_to_hsl(h, s, v) {
     _l /= 2;
 
     return {
-        h: _h,
-        s: _s,
-        l: _l
+        hue: _h,
+        saturation: _s,
+        lightness: _l
     };
 }
 
-export function hsl_to_hsb(h, s, l) {
-    if (arguments.length === 1) {
-        s = h.s, l = h.l, h = h.h;
-    };
-    
+export function hsl_to_hsb(h) {
+    var s = data.saturation
+    var l = data.lightness
+    var h = data.hue;
+
     var _h = h,
         _s,
         _v;
@@ -31,19 +31,27 @@ export function hsl_to_hsb(h, s, l) {
     _s = (2 * s) / (l + s);
 
     return {
-        h: _h,
-        s: _s,
-        v: _v
+        hue: _h,
+        saturation: _s,
+        brightness: _v
     };
 }
 
-export function hsl_to_css(h, s, l) {
-    if (arguments.length === 1) {
-        s = h.s, l = h.l, h = h.h;
-    }
-    return 'hsl('+ Math.round(h*360) +', '+ Math.round(s*100) +'%, '+ Math.round(l*100)+'%)'  
+export function hsl_to_css(data) {
+    var s = data.saturation;
+    var l = data.lightness;
+    var h = data.hue;
+    return 'hsl('+ Math.round(h*360) +', '+ Math.round(s*100) +'%, '+ Math.round(l*100)+'%)'
 }
 
-export function hsb_to_css(h, s, b) {
-    return hsl_to_css(hsb_to_hsl(h, s, b))
+export function hsb_to_css(data) {
+    return hsl_to_css(hsb_to_hsl(data))
+}
+
+export function hsb_to_css_with_lightness(data, lightness = 0.5) {
+
+    var hsl = hsb_to_hsl(data)
+    data.lightness = lightness
+
+    return hsl_to_css(data)
 }
