b40cb7ae3b7f04588db85ef72ef4ac7a66f4b506
Riccardo Di Virgilio <rdv@rdv.local>
2017-12-28 13:10:27 +0100

    random saturation

--- rdvhome/utils/colors.py
+++ rdvhome/utils/colors.py
@@ -81,4 +81,7 @@ def hsb_to_color(h, s, b):
     )
 
 def random_color():
-    return HSB(hue = random.random(), saturation = 1)
+    return HSB(
+        hue = random.random(), 
+        saturation = 0.5 + random.random() * 0.5
+    )
