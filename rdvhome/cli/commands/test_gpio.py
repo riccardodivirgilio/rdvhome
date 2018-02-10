4ce5419952e2dbc865cb7e60ac7c1112c9476c50
riccardodivirgilio <riccardodivirgilio@gmail.com>
2018-02-10 09:57:54 +0100

    adding relay script

--- rdvhome/cli/commands/test_gpio.py
+++ rdvhome/cli/commands/test_gpio.py
@@ -3,19 +3,31 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from rdvhome.cli.utils import SimpleCommand
+from rdvhome.utils.functional import iterate
 
 import time
 
-def test(number = 3):
+LED1   = 18
+LED2   = 23
+RELAY1 = 16
+
+def test(number = [RELAY1]):
+
+
     import RPi.GPIO as GPIO
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-    GPIO.setup(number, GPIO.OUT)
-    print("LED on")
-    GPIO.output(number, GPIO.HIGH)
-    time.sleep(1)
-    print("LED off")
-    GPIO.output(number, GPIO.LOW)
+
+    for n in iterate(number):
+        GPIO.setup(n, GPIO.OUT)
+        print("LED %s on" % n)
+        GPIO.output(n, GPIO.HIGH)
+
+    time.sleep(2)
+
+    for n in iterate(number):
+        print("LED %s off" % n)
+        GPIO.output(n, GPIO.LOW)
 
 class Command(SimpleCommand):
 
