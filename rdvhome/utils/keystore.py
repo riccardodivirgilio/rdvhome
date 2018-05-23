36dd5db79ef682ad0ec1d5dba0a785aa2dbc9672
riccardodivirgilio <riccardodivirgilio@gmail.com>
2018-05-23 17:21:12 +0200

    adding encoder

--- rdvhome/utils/keystore.py
+++ rdvhome/utils/keystore.py
@@ -5,6 +5,9 @@ import json
 
 class KeyStore(object):
 
+    encoder   = json
+    extension = '.json'
+
     def __init__(self, path, prefix = None, version = None, create = True):
         self.path    = path
         self.prefix  = prefix
@@ -15,23 +18,23 @@ class KeyStore(object):
                 os.makedirs(self.path)
             except FileExistsError:
                 pass
-                
+
     def path_for_key(self, key, prefix = None, version = None):
         return os.path.join(
             self.path, '-'.join(filter(None, (
                 prefix or self.prefix, 
                 str(key), 
                 version or self.version
-            ))) + '.json'
+            ))) + self.extension
         )
 
     def get(self, key, default = None, **opts):
         try:
             with open(self.path_for_key(key, **opts)) as f:
-                return json.load(f)
+                return self.encoder.load(f)
         except FileNotFoundError:
             return default
 
     def set(self, key, value, **opts):
         with open(self.path_for_key(key, **opts), 'w') as f:
-            json.dump(value, f)
+            self.encoder.dump(value, f)
