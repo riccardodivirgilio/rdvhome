26d63a4fa008b977b3fed0c4855abc409d628c4a
riccardodivirgilio <riccardodivirgilio@gmail.com>
2018-06-05 23:28:52 +0200

    adding homekit example code

--- rdvhome/cli/commands/run.py
+++ rdvhome/cli/commands/run.py
@@ -7,9 +7,12 @@ from aiohttp import web
 from rdvhome.app import app
 from rdvhome.cli.utils import SimpleCommand
 from rdvhome.conf import settings
+from rdvhome.homekit import driver
 from rdvhome.switches import switches
 from rdvhome.utils.async import run_all
 from rdvhome.utils.process import system_open
+
+import signal
 
 async def log(event):
     print(event.id, event.get('on', None) and 'on' or 'off', event.get('color', None) or '')
@@ -32,4 +35,19 @@ class Command(SimpleCommand):
         if auto_open:
             system_open('http://%s:%s' % (address, port))
 
-        web.run_app(app, port=port)
+        # We want SIGTERM (kill) to be handled by the driver itself,
+        # so that it can gracefully stop the accessory, server and advertising.
+        signal.signal(signal.SIGTERM, driver.signal_handler)
+
+        #code borrowed from pyhap
+        try:
+            driver.add_job(driver._do_start)
+            web.run_app(app, port=port)
+        except KeyboardInterrupt:
+            driver.loop.call_soon_threadsafe(
+                driver.loop.create_task,
+                driver.async_stop()
+            )
+            driver.loop.run_forever()
+        finally:
+            driver.loop.close()
---
+++ rdvhome/homekit.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from pyhap.accessory import Accessory, Bridge
+from pyhap.accessory_driver import AccessoryDriver
+from pyhap.const import CATEGORY_LIGHTBULB
+
+import asyncio
+import logging
+
+"""An example of how to setup and start an Accessory.
+This is:
+1. Create the Accessory object you want.
+2. Add it to an AccessoryDriver, which will advertise it on the local network,
+    setup a server to answer client queries, etc.
+"""
+
+logging.basicConfig(level=logging.INFO)
+
+class LightBulb(Accessory):
+
+    category = CATEGORY_LIGHTBULB
+
+    @classmethod
+    def _gpio_setup(_cls, pin):
+        return
+        if GPIO.getmode() is None:
+            GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(pin, GPIO.OUT)
+
+    def __init__(self, *args, pin=11, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        serv_light = self.add_preload_service('Lightbulb')
+        self.char_on = serv_light.configure_char(
+            'On', setter_callback=self.set_bulb)
+
+        self.pin = pin
+        self._gpio_setup(pin)
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        self._gpio_setup(self.pin)
+
+    #@Accessory.run_at_interval(1)
+    #def run(self):
+    #    self.char_on.set_value(random.randint(0, 1))
+
+    def set_bulb(self, value):
+
+        print(value)
+
+        return
+
+        if value:
+            GPIO.output(self.pin, GPIO.HIGH)
+        else:
+            GPIO.output(self.pin, GPIO.LOW)
+
+    def stop(self):
+        super().stop()
+        #GPIO.cleanup()
+
+def get_bridge(driver):
+    """Call this method to get a Bridge instead of a standalone accessory."""
+    bridge = Bridge(driver, 'Bridge')
+    temp_sensor = LightBulb(driver, 'Sensor 2')
+    temp_sensor2 = LightBulb(driver, 'Sensor 1')
+    bridge.add_accessory(temp_sensor)
+    bridge.add_accessory(temp_sensor2)
+
+    return bridge
+
+# Start the accessory on port 51826
+driver = AccessoryDriver(port=51826, loop = asyncio.get_event_loop())
+
+# Change `get_accessory` to `get_bridge` if you want to run a Bridge.
+driver.add_accessory(accessory=LightBulb(driver, 'MyTempSensor'))
