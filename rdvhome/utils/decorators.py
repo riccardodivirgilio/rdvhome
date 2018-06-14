5aa139868ecc7058971e538bb7e7e18bdc83d94e
riccardodivirgilio <riccardodivirgilio@gmail.com>
2018-06-15 01:13:07 +0200

    adding debounce

--- rdvhome/utils/decorators.py
+++ rdvhome/utils/decorators.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from rdvhome.utils.datastructures import data
 from rdvhome.utils.functional import composition
-
+import time
 def decorate(*func):
     comp = composition(*func)
     def multipass(fn):
@@ -15,3 +15,19 @@ def decorate(*func):
 
 to_tuple = decorate(tuple)
 to_data  = decorate(data)
+
+def debounce(s):
+    """Decorator ensures function that can only be called once every `s` seconds.
+    """
+    def decorate(f):
+        t = None
+
+        def wrapped(*args, **kwargs):
+            nonlocal t
+            t_ = time.time()
+            if t is None or t_ - t >= s:
+                result = f(*args, **kwargs)
+                t = time.time()
+                return result
+        return wrapped
+    return decorate
