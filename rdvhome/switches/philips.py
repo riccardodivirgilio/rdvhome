268a2a1634be892ff0644af4a426ea1107067ce2
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2018-08-18 13:00:46 +0200

    pooling the hue every 3 seconds

--- rdvhome/switches/philips.py
+++ rdvhome/switches/philips.py
@@ -4,19 +4,25 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from pyhap.const import CATEGORY_LIGHTBULB
 
+from rdvhome.switches import switches
 from rdvhome.switches.base import capabilities, HomekitSwitch, Switch
 from rdvhome.utils import json
-from rdvhome.utils.colors import color_to_homekit, color_to_philips, homekit_to_color, philips_to_color, to_color
+from rdvhome.utils.colors import color_to_homekit, color_to_philips, homekit_to_color, HSB, philips_to_color, to_color
 from rdvhome.utils.datastructures import data
-from rdvhome.utils.decorators import to_data
 from rdvhome.utils.gpio import get_gpio
 from rdvhome.utils.keystore import KeyStore
-from rdvhome.switches.base import capabilities, Switch
-from rdvhome.switches import switches
 
 import aiohttp
 import asyncio
 import time
+import traceback
+
+def remove_none(**d):
+    return d.__class__(
+        (key, value)
+        for key, value in d.items()
+        if value is not None
+    )
 
 def debounce(s):
     """Decorator ensures function that can only be called once every `s` seconds.
@@ -84,8 +90,10 @@ class HomekitLight(HomekitSwitch):
         await super().on_event(event)
 
         for attr, value in color_to_homekit(event).items():
-            print(attr, value)
-            getattr(self, '%s_service' % attr).set_value(value)
+            try:
+                getattr(self, '%s_service' % attr).set_value(value)
+            except AttributeError:
+                pass
 
 class PhilipsBase(Switch):
 
@@ -97,6 +105,22 @@ class PhilipsBase(Switch):
         self.username    = username
 
         super().__init__(id, **opts)
+
+    async def api_request(self, path = '', payload = None):
+
+        path = 'http://%s/api/%s/lights/%s' % (
+            self.ipaddress,
+            self.username,
+            path
+        )
+
+        async with aiohttp.ClientSession() as session:
+            if payload:
+                async with session.put(path, json = payload) as response:
+                    return await response.json(loads = json.loads)
+            else:
+                async with session.get(path) as response:
+                    return await response.json(loads = json.loads)
 
 class Light(PhilipsBase):
 
@@ -111,17 +135,13 @@ class Light(PhilipsBase):
             brightness = bool(self.philips_id),
         )
 
-    @property
-    @to_data
-    def philips_settings(self):
-
-        if not self.gpio_relay:
-            yield "on", False
-
-        if self.philips_id:
-            yield "brightness", 1
-            yield "hue",        0.5
-            yield "saturation", 1
+    philips_default_settings = data(
+        on         = False,
+        allow_on   = True,
+        hue        = 0.5,
+        brightness = 1,
+        saturation = 1,
+    )
 
     def __init__(self, id, philips_id = None, gpio_relay = None, gpio_status = None, **opts):
 
@@ -133,24 +153,8 @@ class Light(PhilipsBase):
 
         super().__init__(id, **opts)
 
-    async def api_request(self, path = '', payload = None):
-
-        assert self.ipaddress and self.username and self.philips_id
-
-        path = 'http://%s/api/%s/lights/%s%s' % (
-            self.ipaddress,
-            self.username,
-            self.philips_id,
-            path
-        )
-
-        async with aiohttp.ClientSession() as session:
-            if payload:
-                async with session.put(path, json = payload) as response:
-                    return await response.json(loads = json.loads)
-            else:
-                async with session.get(path) as response:
-                    return await response.json(loads = json.loads)                
+    async def api_request(self, path = '', *args, **opts):
+        return await super().api_request(path = '%s%s' % (self.philips_id, path), *args, **opts)
 
     async def watch(self):
         if self.gpio_status:
@@ -202,79 +206,144 @@ class Light(PhilipsBase):
 
         return not await gpio.input(self.gpio_status)
 
-    async def status(self, allow_philips = True):
+    async def saved_status(self):
+        return await self.store.get(self.id, self.philips_default_settings)
+
+    async def update_status(self, **payload):
+        payload = data(
+            await self.saved_status(),
+            **payload
+        )
+        await self.store.set(self.id, payload)
+
+    async def status(self):
 
         if self.gpio_relay:
             defaults = data(on = await self.raspberry_status())
         else:
             defaults = data()
 
-        if self.philips_id and allow_philips:
+        if self.philips_id:
 
-            if self.username:
-                response = await self.api_request()
-                response = data(
-                    on = response.state.reachable and response.state.on or False,
-                    color = philips_to_color(
-                        hue        = float(response.state.hue),
-                        saturation = float(response.state.sat),
-                        brightness = float(response.state.bri),
-                    ),
-                    allow_on = response.state.reachable or self.gpio_relay,
-                )
-            else:
-                #debug mode
-                response = await self.store.get(self.id, self.philips_settings)
-
+            response = await self.saved_status()
             response.update(defaults)
+
             return await self.send(**response)
 
         return await self.send(**defaults)
 
-    @to_data
-    def parse_command(self, on = None, color = None):
-
-        if on is not None:
-            yield 'on', bool(on)
-
-        if color is not None:
-            if self.username:
-                yield from color_to_philips(color).items()
-            else:
-                yield from to_color(color).serialize().items()
-
     async def switch(self, on = None, color = None):
-        payload = self.parse_command(on, color)
 
-        if 'on' in payload and self.gpio_relay:
-            on = payload.pop('on')
+        if on is not None and self.gpio_relay:
             if not on == await self.raspberry_status():
                 await self.raspberry_switch(on)
 
-        if payload:
+        if self.philips_id and self.username:
 
-            if self.philips_id:
-                if self.username:
-                    await self.api_request('/state', payload)
-                else:
-                    #debug mode
-                    payload = data(
-                        await self.store.get(self.id, self.philips_settings),
-                        **payload
-                    )
-                    await self.store.set(self.id, payload)
+            request = data()
+
+            if on is not None:
+                request.on = bool(on)
+
+            if color is not None:
+                request.update(color_to_philips(color))
+
+            if request:
+                await self.api_request(path = '/state', payload = request)
+
+        await self.update_status(**remove_none(on = on, **to_color(color).serialize()))
 
         return await self.send(on = on, color = color, full = False)
 
 class PhilipsPoolControl(PhilipsBase):
-    
+
     default_capabilities = capabilities(visibility = False)
+
+    philips_initial_color = HSB(
+        hue        = 0.12845044632639047,
+        saturation = 0.5511811023622047,
+        brightness = 1
+    )
 
     async def status(self):
         return await self.send()
 
-    async def watch(self):
+    async def watch(self, interval = 3):
 
-        lights = switches.filter(lambda switch: getattr(switch, 'philips_id', None))
+        if not self.username:
+            return
 
-        print(lights)
+        lights = {
+            str(light.philips_id): light
+            for light in switches.filter(
+                lambda switch: getattr(switch, 'philips_id', None)
+            )
+        }
+
+        while True:
+
+            try:
+                await self.update_lights(lights)
+            except Exception as e:
+                print(e)
+                traceback.print_tb(e.__traceback__)
+
+            await asyncio.sleep(interval)
+
+    async def update_lights(self, lights):
+
+        payload = await self.api_request()
+
+        for philips_id, response in payload.items():
+
+            light = lights.get(philips_id)
+
+            if light:
+
+                is_changed = False
+
+                current_on       = response.state.reachable and response.state.on or False
+                current_allow_on = response.state.reachable or bool(light.gpio_relay)
+                current_color    = philips_to_color(
+                    hue          = float(response.state.hue),
+                    saturation   = float(response.state.sat),
+                    brightness   = float(response.state.bri),
+                )
+
+                saved = await light.saved_status()
+
+                saved_on       = saved.on
+                saved_allow_on = saved.allow_on
+                saved_color    = to_color(saved)
+
+                if current_color == self.philips_initial_color:
+
+                    if response.state.reachable:
+
+                        if saved_color == self.philips_initial_color:
+                            saved_color = to_color(light.philips_default_settings)
+
+                        await light.switch(color = saved_color)
+                        current_color = saved_color
+                        is_changed = True
+                        print('INITIAL_COLOR', light, response.state)
+
+                if not light.gpio_relay and (not current_on == saved_on or not current_allow_on == saved_allow_on):
+                    is_changed = True
+
+                    print('DIFFERENT ON', light)
+
+                if not current_color == saved_color:
+                    is_changed = True
+
+                    print('DIFFERENT COLOR', light)
+
+                if is_changed:
+
+                    # if something changed, then we need to update the status and trigger notifications
+                    await light.update_status(
+                        on = current_on,
+                        allow_on = current_allow_on,
+                        **current_color.serialize()
+                    )
+                    await light.status()
