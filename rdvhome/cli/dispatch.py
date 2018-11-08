dcc4b77a5aa412c7401d2c33782969c2179920b6
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2018-11-08 20:13:23 +0100

    migrating to rpy, adding samsung tv

--- rdvhome/cli/dispatch.py
+++ rdvhome/cli/dispatch.py
@@ -2,43 +2,9 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-from rdvhome.cli.utils import discover_with_convention, SimpleCommand
-from rdvhome.utils.importutils import import_string
+from rpy.cli.dispatch import DispatchCommand as _DispatchCommand
 
-import sys
 
-class DispatchCommand(SimpleCommand):
+class DispatchCommand(_DispatchCommand):
 
     modules = ['rdvhome.cli.commands']
-    class_name = 'Command'
-
-    default_command = None
-
-    def subcommands(self):
-        return discover_with_convention(self.modules, self.class_name)
-
-    def handle(self, attr = None):
-        all_commands = self.subcommands()
-
-        if attr is None and self.default_command:
-            attr = self.default_command
-
-        if attr in all_commands:
-            return import_string(all_commands[attr])(self.subcommand_args()).main()
-
-        self.print('Select one of the following commands:')
-        for command in sorted(all_commands.keys()):
-            self.print(' -', command)
-
-        sys.exit(1)
-
-    def subcommand_args(self):
-        argv = list(self.argv)
-        if len(argv) > 1:
-            argv.pop(1)
-        return argv
-
-    def main(self):
-        if len(self.argv) > 1 and self.argv[1]:
-            return self.handle(self.argv[1])
-        return self.handle()
--- rdvhome/cli/main.py
+++ rdvhome/cli/main.py
@@ -4,12 +4,13 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 import sys
 
+
 def execute_from_command_line(argv = None, **opts):
 
     if sys.version_info[0] == 2:
         raise NotImplementedError('There is no support for python2. Please run python3.')
 
-    from rdvhome.utils.require import require_module
+    from rpy.functions.require import require_module
     from rdvhome.conf import settings
 
     settings.update(opts)
