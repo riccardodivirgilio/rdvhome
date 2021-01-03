fbddc4e4288ac8c7f7bb89b2a39b18b0eb895ec1
Riccardo Di Virgilio <riccardod@wolfram.com>
2021-01-04 00:34:30 +0100

    new app

---
+++ rdvhome/native/src/components/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to rdv home"/>
+        <ListView for="item in listOfItems" @itemTap="onItemTap">
+          <v-template>
+            <!-- Shows the list item label in the default color and style. -->
+            <DockLayout class="line" stretchLastChild="true" >
+              <Label class="icon" text="💡" dock="left" />
+              <Switch class="toggle" checked="true" dock="right"  />
+              <Label class="name" :text="item.text" dock="bottom" />
+            </DockLayout>
+          </v-template>
+        </ListView>
+    </Page>
+</template>
+
+<script >
+  export default {
+    data() {
+      return {
+        msg: 'Welcome to RdvHome!',
+        listOfItems: [{'text': 'Bedroom light'}, {'text': 'Bedroom led'}]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    .icon,
+    .name {
+        height: 60;
+    }
+    .icon {
+        width: 60;
+        text-align: center;
+
+    }
+    .name {
+        text-align: left;
+        width: auto;
+    }
+    .toggle {
+        margin-right: 10;
+        vertical-align: center;
+
+    }
+</style>
+
+
