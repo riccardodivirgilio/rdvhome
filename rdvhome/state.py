fffc9f5c54778d57f6813b6384a81d9039d8c75c
Riccardo Di Virgilio <riccardod@wolfram.com>
2021-01-15 20:26:39 +0100

    moving state

--- rdvhome/state.py
+++ rdvhome/state.py
@@ -11,11 +11,14 @@ def construct(n, class_path, active=True, **switch):
     return import_string(class_path)(**dict({"ordering": n + 1}, **switch))
 
 
-switches = SwitchList(
-    # lazy construction so that we don't have any problem with recursion
-    lambda: (
-        construct(n, **switch)
-        for n, switch in enumerate(settings.SWITCHES)
-        if switch and switch.get("active", True)
+switches, controls = (
+    SwitchList(
+        # lazy construction so that we don't have any problem with recursion
+        lambda: (
+            construct(n, **switch)
+            for n, switch in enumerate(sett)
+            if switch and switch.get("active", True)
+        )
     )
+    for sett in (settings.SWITCHES, settings.CONTROLS)
 )
