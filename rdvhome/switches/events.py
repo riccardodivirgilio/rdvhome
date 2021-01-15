2f6775b160639cf2103fc89ac816ff22bfd90cf8
Riccardo Di Virgilio <riccardod@wolfram.com>
2021-01-15 23:13:33 +0100

    adding periodic task

--- rdvhome/switches/events.py
+++ rdvhome/switches/events.py
@@ -4,9 +4,14 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from aioreactive.core import AsyncAnonymousObserver, AsyncStream
 from aioreactive.core import subscribe as _subscribe
+import asyncio
+import traceback
 
 
 class EventStream(AsyncStream):
+
+    interval = None
+
     def subscribe(self, func):
         return subscribe(self, func)
 
@@ -14,8 +19,18 @@ class EventStream(AsyncStream):
         pass
 
     async def watch(self):
-        pass
 
+        while self.interval:
+            try:
+                await self.periodic_task()
+            except Exception as e:
+                print(e)
+                traceback.print_tb(e.__traceback__)
+
+            await asyncio.sleep(self.interval)
+
+    async def periodic_task(self):
+        pass
 
 def subscribe(observable, func):
     return _subscribe(observable, AsyncAnonymousObserver(func))
