3cea5a93abf522cca3c15a6d281a9d9fa87dd930
Riccardo Di Virgilio <riccardod@wolfram.com>
2021-01-16 01:49:35 +0100

    adding power for gpio

--- rdvhome/controllers/gpio.py
+++ rdvhome/controllers/gpio.py
@@ -22,10 +22,21 @@ class Controller(BaseController):
             for id, key in mapping.items()
         }
 
-    async def switch_power(self, switches, power):
-        mapping = self.get_value_for_property('power', 'gpio_power')
-        await wait_all(
-            self.api_request(path="/%s/state" % mapping[switch.id], payload=dict(on = bool(power)))
-            for switch in switches
-        )
+    def generate_power_path(self, switches):
 
+        mapping = self.get_value_for_property('power', 'gpio_relay')
+
+        for s in switches:
+            yield 'low'
+            yield mapping[s.id]
+
+        yield 'wait'
+        yield '25'
+
+        for s in switches:
+            yield 'high'
+            yield mapping[s.id]
+
+    async def switch_power(self, switches, power):
+        await self.api_request('/%s/' % "/".join(self.generate_power_path(switches)))
+        await super().switch_power(switches, power)
