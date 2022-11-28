2b15bf5d3834efcf197871a794cc40bd41c0218c
Riccardo Di Virgilio <riccardod@wolfram.com>
2022-11-28 07:58:12 +0100

    adding use effect and loading

--- raycast/src/index.tsx
+++ raycast/src/index.tsx
@@ -1,65 +1,60 @@
-import { ActionPanel, Action, List, Icon } from "@raycast/api"
-import { useFetch } from "@raycast/utils"
+import { ActionPanel, Action, List, Icon } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
 import { showToast, Toast } from "@raycast/api";
 
-import { useState } from "react"
+import { useState, useEffect } from "react";
 
 //https://github.com/raycast/extensions/blob/main/examples/todo-list/src/index.tsx
 
 export async function run_toggle(toggle, setToggles) {
-
-const toast = await showToast({
+  const toast = await showToast({
     style: Toast.Style.Animated,
     title: `Switched ${toggle.on ? "off" : "on"} ${toggle.name}`,
   });
 
-  await fetch(get_api("switch/" + toggle.id + (toggle.on ? "/off" : "/on")))
-  await updateState(setToggles)
+  await fetch(get_api("switch/" + toggle.id + (toggle.on ? "/off" : "/on")));
+  await updateState(setToggles);
 
-  await toast.hide()
+  await toast.hide();
 }
 
 function get_api(path) {
-  return "http://rdvhome.local:8500/" + path
+  return "http://rdvhome.local:8500/" + path;
 }
 
 /** Parse the response from the fetch query into something we can display */
 async function updateState(setToggles) {
-
-  const json = await (await fetch(get_api("switch"))).json()
+  const json = await (await fetch(get_api("switch"))).json();
 
   if ("reason" in json) {
-    throw new Error(json.reason)
+    throw new Error(json.reason);
   }
 
-  setToggles(Object.values(json.switches || []).filter(toggle => toggle.allow_visibility))
+  await setToggles(Object.values(json.switches || []).filter((toggle) => toggle.allow_visibility));
 }
 
-
 export default function Command() {
-
-
   const [toggles, setToggles] = useState([]);
 
-  updateState(setToggles);
+  useEffect(() => updateState(setToggles), []); // Or [] if effect doesn't need props or state
 
-  const on = toggles.filter(t => t.on)
-  const off = toggles.filter(t => ! t.on)
+  const on = toggles.filter((t) => t.on);
+  const off = toggles.filter((t) => !t.on);
 
   return (
-    <List searchBarPlaceholder="Search lights...">
+    <List isLoading={toggles.length == 0} searchBarPlaceholder="Search lights...">
       <List.Section title="On" subtitle={on.length + ""}>
-        {on.map(toggle => (
+        {on.map((toggle) => (
           <SearchListItem key={toggle.name} toggle={toggle} setToggles={setToggles} />
         ))}
       </List.Section>
       <List.Section title="Off" subtitle={off.length + ""}>
-        {off.map(toggle => (
+        {off.map((toggle) => (
           <SearchListItem key={toggle.name} toggle={toggle} setToggles={setToggles} />
         ))}
       </List.Section>
     </List>
-  )
+  );
 }
 
 function SearchListItem({ toggle, setToggles }) {
@@ -68,7 +63,7 @@ function SearchListItem({ toggle, setToggles }) {
       id={toggle.id}
       title={toggle.name}
       subtitle={toggle.icon}
-      accessories={toggle.alias.map(a => ({ text: a }))}
+      accessories={toggle.alias.map((a) => ({ text: a }))}
       icon={toggle.on ? Icon.CheckCircle : Icon.Circle}
       actions={
         <ActionPanel>
@@ -88,5 +83,5 @@ function SearchListItem({ toggle, setToggles }) {
         </ActionPanel>
       }
     />
-  )
+  );
 }
